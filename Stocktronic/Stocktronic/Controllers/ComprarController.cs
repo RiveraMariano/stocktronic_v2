18ad0176dd2a0c746deb285d38c3ab008ec91eeb
Mariano Rivera <rivveramariano@gmail.com>
2021-11-13 13:21:56 -0600

    Vistas de formulario de pago

---
+++ Stocktronic/Stocktronic/Controllers/ComprarController.cs
@@ -0,0 +1,28 @@
+﻿using Stocktronic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Stocktronic.Controllers
+{
+    public class ComprarController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            try
+            {
+                CarritoModel carrito = new CarritoModel();
+                var productosAgregados = carrito.ListarProductosAgregados();
+                return View(productosAgregados);
+            }
+            catch (Exception)
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+        }
+
+    }
+}
